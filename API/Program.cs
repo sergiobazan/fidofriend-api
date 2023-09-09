4a31a70090451f48819eb9474336d617d753f802
Sergio Bazan <drakeb2008@hotmail.com>
2023-09-09 18:54:44 -0500

    add swagger ui

--- API/Program.cs
+++ API/Program.cs
@@ -48,11 +48,8 @@ builder.Services.AddAutoMapper(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
